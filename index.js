339d4a1a3ca19e47ccf0c9d20c363bc93eec295b
hofkj331 <>
2021-05-18 00:31:43 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,25 @@
+let dateA	= "Monday 2019-03-18";
+let timeA	= "10:05:34 AM";
+let astronautCount =	7;
+let astronautStatus	= "ready";
+let averageAstronautMassKg =	80.7;
+let crewMassKg =	astronautCount * averageAstronautMassKg;
+let fuelMassKg =	760000;
+let shuttleMassKg =	74842.31;
+let totalMassKg =	crewMassKg + fuelMassKg + shuttleMassKg;
+let fuelTempCelsius =	-225;
+let fuelLevel	= "100%";
+let weatherStatus =	"clear";
+
+console.log(dateA);
+console.log(timeA);
+console.log(astronautCount);
+console.log(astronautStatus);
+console.log(averageAstronautMassKg);
+console.log(crewMassKg);
+console.log(fuelMassKg);
+console.log(totalMassKg);
+console.log(fuelTempCelsius);
+console.log(fuelLevel);
+console.log(weatherStatus);
+
